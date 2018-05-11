cb1d50cea3f167a821c2e0fe437473d811fabb5d
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2018-05-11 16:25:01 -0400

    update

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -12,19 +12,7 @@ $('ul').on('click', 'span', function(event){
 });
 
   
-  $("input[type='text']").keypress(function(event){
-  	if(event.which === 13){
-  		//grabbing new todo text from input
-  		var newToDoText = $(this).val();
-
-  		// WRONG // $("input[type='text']") === ""; WRONG
- 		 $(this).val(""); //CORRECT
-
-  		//create a new li and add to ul
-  	 	$("ul").append("<li><span><i class ='fa fa-trash'></span></i> " + newToDoText + "</li>");
-  	}
-  })
-
+ 
 //plus sign functionality
   $("#plusSpan").on('click', function () {
   	$("input[type='text']").fadeToggle()
