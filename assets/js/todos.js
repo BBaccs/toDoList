82a3b869465e3feb67054bd60757477acd8bc7bf
Brian Baccarella <34583054+BBaccs@users.noreply.github.com>
2018-05-11 16:25:33 -0400

    functionality bug fixed

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -11,7 +11,19 @@ $('ul').on('click', 'span', function(event){
 	event.stopPropagation();
 });
 
-  
+   $("input[type='text']").keypress(function(event){
+  	if(event.which === 13){
+  		//grabbing new todo text from input
+  		var newToDoText = $(this).val();
+
+  		// WRONG // $("input[type='text']") === ""; WRONG
+ 		 $(this).val(""); //CORRECT
+
+  		//create a new li and add to ul
+  	 	$("ul").append("<li><span><i class ='fa fa-trash'></span></i> " + newToDoText + "</li>");
+  	}
+  })
+
  
 //plus sign functionality
   $("#plusSpan").on('click', function () {
